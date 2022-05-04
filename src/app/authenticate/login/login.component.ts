78b3d381d6ff22dd0a6844efc10a5d8e7ac2b895
naila62452 <naila.nosheen.uos@gmail.com>
2022-05-04 19:33:41 +0100

    login

--- src/app/authenticate/login/login.component.ts
+++ src/app/authenticate/login/login.component.ts
@@ -26,7 +26,6 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     this.teacherService.loginUser(this.loginForm.value, true)
       .subscribe(res => {
-        if (res.user.isVerified == true) {
           localStorage.setItem('token', res.token);
           localStorage.setItem('name', res.user.name);
           localStorage.setItem('id', res.user._id);
@@ -38,13 +37,26 @@ export class LoginComponent implements OnInit {
           });
           this.loginForm.reset();
           this.router.navigate(['/user/profile']);
-        }
-        else {
-          this._snackBar.open("Please verify your email first", "Ok", {
-            duration: 5000,
-            panelClass: ['blue-snackbar']
-          });
-        }
+          
+        // if (res.user.isVerified == true) {
+        //   localStorage.setItem('token', res.token);
+        //   localStorage.setItem('name', res.user.name);
+        //   localStorage.setItem('id', res.user._id);
+        //   localStorage.setItem("isLoggedIn", "true");
+
+        //   this._snackBar.open("You have logged in successfully", "Ok", {
+        //     duration: 5000,
+        //     panelClass: ['blue-snackbar']
+        //   });
+        //   this.loginForm.reset();
+        //   this.router.navigate(['/user/profile']);
+        // }
+        // else {
+        //   this._snackBar.open("Please verify your email first", "Ok", {
+        //     duration: 5000,
+        //     panelClass: ['blue-snackbar']
+        //   });
+        // }
       },
         err => {
           this._snackBar.open("Incorrect Email or Password", "Ok", {
