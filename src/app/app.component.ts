6ada368a24943ed607cf9c680a685149577c3e64
naila62452 <naila.nosheen.uos@gmail.com>
2022-06-28 14:27:50 +0100

    typography

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateService } from "@ngx-translate/core";
-import { ViewChild } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
-import { delay, filter } from 'rxjs/operators';
-import { NavigationEnd, Router } from '@angular/router';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,15 +9,5 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 export class AppComponent {
   title = 'Seeds';
 
-  constructor(public translate: TranslateService, private observer: BreakpointObserver, private router: Router) {
-    translate.addLangs(['English', 'Español', 'Deutsch', 'Ελληνική']);
-    let language = localStorage.getItem('language')
-    if (language)
-      this.translate.use(language)
-    else translate.setDefaultLang('English');
-  }
-  switchLanguage(lang: string) {
-    this.translate.use(lang)
-    localStorage.setItem('language', lang)
-  }
+  constructor() {}
 }
